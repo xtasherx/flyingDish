dcba49ce5ff91a2b8dcdc1bfb2f20a33e56ca0a9
Tasha <xtasherx@gmail.com>
2021-03-02 21:10:54 -0600

    set up api call to spoonacular

--- client/src/Pages/Landing.js
+++ client/src/Pages/Landing.js
@@ -1,8 +1,43 @@
-import React from "react";
-import Hero from "../Components/Hero";
+
+import React, { useState, useEffect} from 'react';
+import axios from "axios";
+import Header from "../Components/Header";
  const Landing = () => {
+
+   const [userSearch,setUserSearch] = useState("");
+   const [searchResults,setSearchResults] = useState([]);
+
+   
+   const handleChange = e => {
+      setUserSearch(e.target.value);
+      console.log(userSearch);
+}
+
+   const handleClick = () => {
+      // api call to spoonacular 
+      console.log(userSearch)
+      const responseURL = `https://api.spoonacular.com/recipes/complexSearch/?query=${userSearch}&sort=random&instructionsRequired=true&maxReadyTime=20&addRecipeInformation=true&fillIngredients=true&max-used-ingredients=10&number=6&apiKey=4f924dd683af4b90b667d59fcf07d711`;
+      axios.get(responseURL)
+         .then( res => {
+            console.log(res.data.results)
+         })
+}
+
      return (
-        <Hero/>
+      <div id="splash-img" className="hero fullscreen hero-img parallax-img">
+      <div className="hero-body p-0">
+      <Header />
+          <div className="content u-text-center u-flex u-flex-column">
+              <h1 className=" text-red-600 headline-4 title ">We heard you're busy.</h1>
+              <h4 className=" text-light sub-title">So we created an easy way to find recipes that you can whip up in 30 minutes or less. Search for an ingredient, a dish or even a cocktail. We've got you covered.</h4>
+              <div class="form-group">
+                  <input type="search" class="form-group-input searchbox" placeholder="pasta" onChange={handleChange} />
+                  <button class="form-group-btn btn-small search-btn bg-green-600 text-light" onClick={handleClick}>Search</button>
+              </div>
+          </div>
+
+      </div>
+  </div>
      )
 
 }
